d3043ab58ee84e99f7f34bf8bd366f9bd0a5891c
KC Lim <kclim0528@gmail.com>
2025-02-04 05:36:52 -0500

    setup create item button

---
+++ frontend/src/Components/CreateItemButton.tsx
@@ -0,0 +1,9 @@
+
+
+export const CreateItemButton: React.FC = () => {
+    return (
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+        + Create Item
+      </button>
+    );
+  };
