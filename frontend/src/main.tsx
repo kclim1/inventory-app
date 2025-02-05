7d5c4f816a3e128b18ddec579c1a5c80319613b7
KC Lim <kclim0528@gmail.com>
2025-02-05 03:56:44 -0500

    set up redux store to manage global states in main

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {Dashboard} from './Dashboard.tsx'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Dashboard } from "./Dashboard.tsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-    < Dashboard/>
-    </Router>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <Router>
+        <Dashboard />
+      </Router>
+    </Provider>
+  </StrictMode>
+);
