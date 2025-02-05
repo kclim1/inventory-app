1937d5af64d90d057182308e03b41d08375aad45
KC Lim <kclim0528@gmail.com>
2025-02-05 03:57:02 -0500

    set up create item button to open create item form

--- frontend/src/Components/CreateItemButton.tsx
+++ frontend/src/Components/CreateItemButton.tsx
@@ -1,9 +1,15 @@
+import { useAppDispatch } from "../store/hooks";
+import { openCreateItemForm } from "../store/slices/createItemFormSlice";
 
 
-export const CreateItemButton: React.FC = () => {
-    return (
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
-        + Create Item
-      </button>
-    );
-  };
+export const CreateItemButton = () => {
+  const dispatch = useAppDispatch();
+  return (
+    <button
+      onClick={()=>dispatch(openCreateItemForm())}
+      className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+    >
+      + Create Item
+    </button>
+  );
+};
