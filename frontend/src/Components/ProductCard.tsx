504d3ce9c9f9168a820994cdba4158e74679b16c
KC Lim <kclim0528@gmail.com>
2025-02-05 03:57:57 -0500

    hardcoded product card details for styling purposes

--- frontend/src/Components/ProductCard.tsx
+++ frontend/src/Components/ProductCard.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { DeleteItemButton } from "./DeleteItemButton";
+
 
 // Define interface for props
 interface ProductCardProps {
@@ -14,16 +15,15 @@ export const ProductCard = ({ productName, description, price }: ProductCardProp
   return (
     <div className="bg-white shadow-md rounded-lg p-4 w-sm">
       <h2 className="font-semibold text-xl mb-2">Sample Product</h2>
-      <p className="text-gray-600">Description here...</p>
+      <p className="text-gray-600 truncate">Description here...</p>
       <div className="flex mt-4 justify-between items-center">
         <span className="text-gray-800 font-bold">$ 20.00</span>
         <div className="buttonContainer flex space-x-2 p-2">
-          <button className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg">
+          <button className="bg-gray-500 hover:bg-gray-600 text-white px-4 py-2 rounded-lg">
             Edit
           </button>
-          <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg">
-            Delete
-          </button>
+          {/* import delete item button below */}
+          <DeleteItemButton/>
         </div>
       </div>
     </div>
