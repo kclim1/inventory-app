ecb43f687785af2dcb752a71dc2cf5baac70d3a5
KC Lim <kclim0528@gmail.com>
2025-02-06 21:55:22 +0800

    setup sequelize to sync with database and mounted middleware to parse json and forms

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,15 +3,27 @@ export {};
 const express = require('express');
 const axios = require('axios')
 const dotenv = require('dotenv').config()
-const { request: Req } = require('express')
-const { response: Res } = require('express')
 const app = express();
 const PORT = process.env.PORT
+const itemRoutes = require('./routes/itemRoutes')
+const sequelize = require('./models/config')
 
-app.get('/', async(req: typeof Req, res: typeof Res) => {
-    res.send("welcome to arkmind")
-    console.log("sample button clicked !")
-})
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+
+
+// Sync models with the database
+sequelize.sync({ force: false })  // Use 'force: true' only if you want to drop tables and recreate them
+  .then(() => {
+    console.log('Database synchronized!');
+  })
+  .catch((error: unknown) => {
+    console.error('Error syncing database:', error);
+  });
+
+
+// appends / api/items to all routes
+app.use('/api/items',itemRoutes)
 
 app.listen(PORT,()=>{
     console.log(`server running on port ${PORT}`)
